f935f9424d2a7226fd2d76dd821dc10c6033ab4d
ortsion <ortsionm@gmail.com>
2021-12-22 20:36:02 +0200

    first load ex 3

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function task3 (){
+    let num1 = prompt("please enter a number" );
+let num2 = prompt("please enter another number" );
+const res = Math.round(Number(num1) / Number(num2));
+
+alert ("result is " + res)
+}
+
